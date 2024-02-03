f1d5062f990edf1976a64486b78f5846fd97c076
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2024-02-03 16:55:21 +0100

    Reset default vite css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react'
 import { CurriculumForm } from './CurriculumForm'
 import { Curriculum } from './Curriculum'
-import '../styles/App.css'
 
 function App() {
   const [curriculum, setCurriculum] = useState({ name: "Winston", surname: "Churchill", value: 'someValue'})
 
   return (
-    <>
+    <div>
       <CurriculumForm someObj={curriculum} someFn={setCurriculum}/>
       <Curriculum anotherObj={curriculum}/>
-    </>
+    </div>
   )
 }
 
